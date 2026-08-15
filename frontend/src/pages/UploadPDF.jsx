45e0b113ed24a582fe91e8c613781caf782e5f86
sreshta <sreshtareddy0722@gmail.com>
2026-08-15 21:12:02 +0530

    Create upload PDF screen

--- frontend/src/pages/UploadPDF.jsx
+++ frontend/src/pages/UploadPDF.jsx
@@ -1,5 +1,31 @@
 function UploadPDF() {
-  return <h1>Upload PDF Page</h1>;
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>📚 Upload Textbook</h1>
+
+      <div
+        style={{
+          marginTop: "30px",
+          padding: "50px",
+          border: "2px dashed #4f46e5",
+          borderRadius: "15px",
+          textAlign: "center",
+        }}
+      >
+        <h2>Drag & Drop PDF Here</h2>
+        <p>or</p>
+
+        <input type="file" accept=".pdf" />
+
+        <br />
+        <br />
+
+        <button>
+          Upload PDF
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default UploadPDF;
