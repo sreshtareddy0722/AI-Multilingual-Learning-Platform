7e9db93ce51e4097c8146999d46a20f8544031cf
sreshta <sreshtareddy0722@gmail.com>
2026-08-15 21:23:11 +0530

    Create analytics screen UI

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -1,5 +1,48 @@
 function Analytics() {
-  return <h1>Analytics Page</h1>;
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>📊 Learning Analytics</h1>
+
+      <div style={card}>
+        <h3>Photosynthesis</h3>
+        <p>90% ✅</p>
+      </div>
+
+      <div style={card}>
+        <h3>Respiration</h3>
+        <p>82% ✅</p>
+      </div>
+
+      <div style={card}>
+        <h3>Cell Division</h3>
+        <p>65% 🟡</p>
+      </div>
+
+      <div style={card}>
+        <h3>Genetics</h3>
+        <p>42% 🔴</p>
+      </div>
+
+      <div
+        style={{
+          marginTop: "30px",
+          padding: "20px",
+          background: "#fff3cd",
+          borderRadius: "10px",
+        }}
+      >
+        <h2>📌 Recommendation</h2>
+        <p>Genetics needs revision.</p>
+      </div>
+    </div>
+  );
 }
+
+const card = {
+  background: "#eef2ff",
+  padding: "20px",
+  marginTop: "15px",
+  borderRadius: "10px",
+};
 
 export default Analytics;
